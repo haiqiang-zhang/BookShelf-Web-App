f23435b971c8ed16d8d4536e4aa51a0af639e5ff
haiqiang-zhang <1552094108@qq.com>
2021-10-16 18:07:55 +0800

    fix bugs
    javascript

--- library/static/book.js
+++ library/static/book.js
@@ -1,11 +1,23 @@
 function read_book(book_id){
     fetch(`/read_a_book?read_book_id=${book_id}`)
-        .then(response => response.text())
-        .then(message => alert(message))
+        .then(response =>{
+            if (response.redirected){
+                window.location.href = response.url;
+            }
+            else {
+                alert(response.text());
+            }
+    })
 }
 
 function favourite_book(book_id){
     fetch(`/favourite_a_book?fav_book_id=${book_id}`)
-        .then(response => response.text())
-        .then(message => alert(message))
+        .then(response =>{
+            if (response.redirected){
+                window.location.href = response.url;
+            }
+            else {
+                alert(response.text());
+            }
+    })
 }
